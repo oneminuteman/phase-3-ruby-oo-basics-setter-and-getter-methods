a33c12a88e9a3cf9baa61e0d02eb65251ee5ded1
Victor <victor.ireri@student.moringaschool.com>
2023-05-22 21:38:29 +0300

    labs

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
-class Person
+person = Person.new("John Doe", "Engineer")
 
-end
+puts person.name
+# Output: "John Doe"
+
+person.name = "Jane Smith"
+puts person.name
+# Output: "Jane Smith"
+
+puts person.job
+# Output: "Engineer"
+
+person.job = "Teacher"
+puts person.job
+# Output: "Teacher"
